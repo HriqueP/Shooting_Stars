49aca5265f2e6c56efcef4a76ede326139ab4b75
HriqueP <hapoletti@gmail.com>
2024-05-16 19:19:03 -0300

    resize canvas and screen

--- js/main.js
+++ js/main.js
@@ -2,3 +2,17 @@ console.log("Shooting Stars");
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Rezise Screen and Canvas
+window.addEventListener("resize", (event) => {
+  init();
+});
+
+function init() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+init();
