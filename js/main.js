c2a051ebc199a14d77712834d417fd8493d46ae5
HriqueP <hapoletti@gmail.com>
2024-05-16 19:43:34 -0300

    classes declarations and drawing the ground

--- js/main.js
+++ js/main.js
@@ -6,11 +6,45 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Rezise Screen and Canvas
+// Global Variables and Constants
+const groundColor = "#11171e";
+const starColor = "#f7fcfd";
+const mountainColors = ["#374652", "#2a3542", "#202a35"];
+
+// Classes Definitions
+class Ground {
+  constructor(x, y, w, h, color) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.color = color;
+  }
+
+  draw() {
+    ctx.fillStyle = groundColor;
+    ctx.fillRect(0, canvas.height - 100, canvas.width, canvas.height);
+  }
+}
+class Mountain {}
+class shootingStar {}
+class star {}
+
+// Classes Objects Declarations
+const ground = new Ground();
+
+// Function Animate Frames Loop
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ground.draw();
+  requestAnimationFrame(animate);
+}
+requestAnimationFrame(animate);
+
+// Resize Screen and Canvas
 window.addEventListener("resize", (event) => {
   init();
 });
-
 function init() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
