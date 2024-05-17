13d4fb258c8483b90320a25353cb28a2d1b89bee
HriqueP <hapoletti@gmail.com>
2024-05-16 23:21:36 -0300

    mountains class declaration and drawing

--- js/main.js
+++ js/main.js
@@ -22,16 +22,81 @@ class Ground {
     ctx.fillRect(0, canvas.height - 100, canvas.width, canvas.height);
   }
 }
-class Mountain {}
+class Mountain {
+  constructor(colors) {
+    this.colors = colors;
+  }
+
+  draw() {
+    // Back
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height - 100);
+    ctx.lineTo(canvas.width, canvas.height - 100);
+    ctx.lineTo(canvas.width / 2, canvas.height - (canvas.height - 120));
+    ctx.lineTo(0, canvas.height - 100);
+    ctx.fillStyle = this.colors[0];
+    ctx.fill();
+    ctx.closePath();
+
+    // Middle
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height - 100);
+    ctx.lineTo(canvas.width / 2, canvas.height - 100);
+    ctx.lineTo(canvas.width / 4, canvas.height / 2 - 120);
+    ctx.lineTo(0, canvas.height - 100);
+    ctx.fillStyle = this.colors[1];
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, canvas.height - 100);
+    ctx.lineTo(canvas.width, canvas.height - 100);
+    ctx.lineTo(canvas.width / 2 + canvas.width / 4, canvas.height / 2 - 120);
+    ctx.lineTo(canvas.width / 2, canvas.height - 100);
+    ctx.fillStyle = this.colors[1];
+    ctx.fill();
+    ctx.closePath();
+
+    // Front
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height - 100);
+    ctx.lineTo(canvas.width / 3, canvas.height - 100);
+    ctx.lineTo(canvas.width / 6, canvas.height / 2 + 120);
+    ctx.lineTo(0, canvas.height - 100);
+    ctx.fillStyle = this.colors[2];
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 3, canvas.height - 100);
+    ctx.lineTo(canvas.width / 3 + canvas.width / 3, canvas.height - 100);
+    ctx.lineTo(canvas.width / 3 + canvas.width / 6, canvas.height / 2 + 120);
+    ctx.lineTo(canvas.width / 3, canvas.height - 100);
+    ctx.fillStyle = this.colors[2];
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 3 + canvas.width / 3, canvas.height - 100);
+    ctx.lineTo(canvas.width, canvas.height - 100);
+    ctx.lineTo(canvas.width / 3 + canvas.width / 2, canvas.height / 2 + 120);
+    ctx.lineTo(canvas.width / 3 + canvas.width / 3, canvas.height - 100);
+    ctx.fillStyle = this.colors[2];
+    ctx.fill();
+    ctx.closePath();
+  }
+}
 class shootingStar {}
 class star {}
 
 // Classes Objects Declarations
 const ground = new Ground(groundColor);
+const mountain = new Mountain(mountainColors);
 
 // Function Animate Frames Loop
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  mountain.draw();
   ground.draw();
   requestAnimationFrame(animate);
 }
